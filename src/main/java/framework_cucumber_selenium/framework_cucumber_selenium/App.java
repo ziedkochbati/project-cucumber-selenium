8246cfeabbdbb3e97e0168e1e47adbf65fd0daa3
ziedkochbati <ziedkochbati@gmail.com>
2021-01-28 22:48:23 +0100

    first commit

---
+++ src/main/java/framework_cucumber_selenium/framework_cucumber_selenium/App.java
@@ -0,0 +1,13 @@
+package framework_cucumber_selenium.framework_cucumber_selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/framework_cucumber_selenium/framework_cucumber_selenium/AppTest.java
@@ -0,0 +1,20 @@
+package framework_cucumber_selenium.framework_cucumber_selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
